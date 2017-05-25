9a5f3bc79dcea924165efd9a9f6808572c51f4b6
cdefanti <connor.defanti@gmail.com>
2017-05-25 01:24:26 -0400

    added base height, tweaked some params

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ import random
 import sys
 
 # params
-res = (256, 256, 3)
+resx = 256
+res = (resx, resx, 3)
 narray = np.zeros(res, dtype=np.int8)
-turb_factor = 4
-noctaves = 4
-npeaks = 2
-peakiness = 1.2
+turb_factor = 8
+noctaves = 8
+npeaks = 8
+peakiness = 1.5
 stratification = 0
+base_height = 0.1
 
 # random params
 offset = (random.uniform(0, 1), random.uniform(0, 1))
@@ -26,8 +28,9 @@ height_to_terrain = {
     0.2      : 'beach',
     0.23     : 'forest',
     0.35     : 'mountain',
-    0.47     : 'snow'
+    0.45     : 'snow'
 }
+
 terrain_to_color = {
     'water'     : [0, 0, 255],
     'beach'     : [237, 201, 175],
@@ -48,11 +51,16 @@ for x in range(res[0]):
         val = (val + 1.0) / 2.0
         val = math.pow(val, peakiness)
 
-        # penalize near border
+        # make peaks
         dist = 1
         for peak in peaks:
             dist = min(dist, math.sqrt((xc - peak[0]) * (xc - peak[0]) + (yc - peak[1]) * (yc - peak[1])))
         val = val * (1.0 - 1.0 * math.pow(dist, 1.0))
+
+        # increase by base height
+        val += base_height
+
+        # discretize steps
         if stratification > 0:
             val = int(val * stratification) / float(stratification)
 
@@ -63,9 +71,9 @@ for x in range(res[0]):
                 height_val = max(height_val, height)
         color = terrain_to_color[height_to_terrain[height_val]]
         new_color = [0, 0, 0]
-        new_color[0] = (color[0] * np.clip(2.0 * val, 0, 1))
-        new_color[1] = (color[1] * np.clip(2.0 * val, 0, 1))
-        new_color[2] = (color[2] * np.clip(2.0 * val, 0, 1))
+        new_color[0] = int(color[0] * np.clip(2.0 * val, 0.1, 1))
+        new_color[1] = int(color[1] * np.clip(2.0 * val, 0.1, 1))
+        new_color[2] = int(color[2] * np.clip(2.0 * val, 0.1, 1))
         narray[x, y, :] = new_color
     sys.stdout.write("\r{:3.2f}% done ".format(100 * float(x) / (res[0])))
     sys.stdout.flush()
